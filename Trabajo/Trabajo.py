13a426721913a3800d2ce31ba4989027ff7a667a
Alejandro <acaro729@soyudemedellin.edu.co>
2024-02-20 18:44:45 -0500

    Commit

--- Trabajo/Trabajo.py
+++ Trabajo/Trabajo.py
@@ -1,12 +1,12 @@
 import math
 class Vehiculo:
-    def __int__(self,velocidad_maxima, kilometraje):
+    def __init__(self,velocidad_maxima, kilometraje):
 
         self.velocidad_maxima = velocidad_maxima
         self.kilometraje = kilometraje
 
 class Punto:
-    def __int__(self,x,y):
+    def __init__(self,x,y):
         self.x = x
         self.y = y
 
@@ -16,3 +16,7 @@ class Punto:
     def mover(self,dx,dy):
         self.x += dx
         self.y += dy
+
+    def calcular_distancia(self,otro_punto):
+        return math.hypot(self.x - otro_punto.x, self.y - otro_punto.y)
+
