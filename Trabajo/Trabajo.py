e052a072e6c513e0f30aba287a4f5a87c0615a54
Alejandro <acaro729@soyudemedellin.edu.co>
2024-02-20 18:59:15 -0500

    Commit

--- Trabajo/Trabajo.py
+++ Trabajo/Trabajo.py
@@ -20,3 +20,26 @@ class Punto:
     def calcular_distancia(self,otro_punto):
         return math.hypot(self.x - otro_punto.x, self.y - otro_punto.y)
 
+class Rectangulo:
+    def __init__(self,esquina1,esquina2):
+        self.esquina1 = esquina1
+        self.esquina2 = esquina2
+
+    def calcular_perimetro(self):
+        largo = abs(self.esquina1.x - self.esquina2.x)
+        ancho = abs(self.esquina1.y - self.esquina2.y)
+        perimetro = 2 * (largo + ancho)
+        print("El perímetro del rectángulo es:",perimetro,"unidades")
+        return perimetro
+
+    def calcular_area(self):
+        largo = abs(self.esquina1.x - self.esquina2.x)
+        ancho = abs(self.esquina1.y - self.esquina2.y)
+        area = largo * ancho
+        print("El área del rectángulo es:",area,"unidades cuadradas")
+        return area
+
+    def es_cuadrado(self):
+        largo = abs(self.esquina1.x - self.esquina2.x)
+        ancho = abs(self.esquina1.y - self.esquina2.y)
+        return largo == ancho
